0d3a7aef369fd506e5045b37ce98672ec0c00fa4
Clive Darracott <clive.darracott@dxc.com>
2022-11-03 16:24:03 +0000

    added datetime

--- hello.py
+++ hello.py
@@ -1,5 +1,21 @@
+from datetime import datetime, date, time
+
 msg = "Hello World"
 print(msg)
 msg = msg+msg
-print('xxx')
-print("yyy")
+
+x = "and %s and %s" % (1, 2)
+print(x)
+print('and %s and %s' % (1, 2))
+
+name = 'Tushar'
+age = 23.23
+
+print(f"Hello, My name is {name} and I'm {age:{1}.{5}} years old.")
+print(f'Hello, My name is {name} and I"m {age} years old.')
+
+d = datetime.now()
+
+e = 'My Is %s' % d
+
+print(e)
